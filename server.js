24467da5dc8c208b00ed160b3c2cbf95abf1999a
Joseph <josephanaebonam@gmail.com>
2025-11-22 21:20:57 +0100

    updated my .env file

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express();
 // CORS configuration
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    // origin: ["http://localhost:5173"],
+    origin: ["https://crudapp-student-m-s.netlify.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
